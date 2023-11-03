82c9ee20c956cd817bd88612eb7c8afcd985fa22
Apiwat Lee <apiwatlee@Apiwats-MacBook-Air.local>
2023-11-03 19:34:50 +0700

    feat:refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,10 @@
-import { useState } from "react";
 import "./App.css";
+import Button from "./components/Button";
+import useCalculator from "./hook/useCalculator";
 
 function App() {
-  const [input, setInput] = useState("0");
-
-  const style = {
-    operator:
-      "border border-gray-200 rounded-lg bg-orange-400 text-white text-3xl",
-    number: "border border-gray-200 rounded-lg bg-gray-200 text-3xl",
-    equal:
-      "border border-gray-200 rounded-lg col-start-4 row-start-2 row-end-6 bg-orange-400 text-white text-3xl",
-  };
-
-  const handleDisplay = (value) => {
-    if (input.toString().charAt(0) === "0") {
-      //ใส่ toString เพื่อแปลง State input เป็นString อีกครั้ง เนื่องจากหลังทำการ onClick handleCalculate แล้ว ตัว State Input จะเป็น Number
-      setInput(value);
-    } else {
-      setInput((prev) => prev + value);
-    }
-  };
-
-  const handleReset = () => {
-    setInput("0");
-  };
-
-  const handleCalculate = () => {
-    const result = eval(input);
-    setInput(result);
-  };
+  const { input, style, handleDisplay, handleReset, handleCalculate } =
+    useCalculator();
 
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-slate-200 ">
@@ -37,57 +13,83 @@ function App() {
           <h1>{input}</h1>
         </div>
         <div className="grid grid-cols-4 grid-row-5 gap-4 border border-gray-200 shadow-xl w-[100%] h-[100%] p-5 bg-white rounded-b-xl">
-          <button className={style.operator} onClick={() => handleDisplay("+")}>
-            +
-          </button>
-          <button className={style.operator} onClick={() => handleDisplay("-")}>
-            -
-          </button>
-          <button className={style.operator} onClick={() => handleDisplay("*")}>
-            &#215;
-          </button>
-          <button className={style.operator} onClick={() => handleDisplay("/")}>
-            &#247;
-          </button>
-          <button className={style.number} onClick={() => handleDisplay("7")}>
-            7
-          </button>
-          <button className={style.number} onClick={() => handleDisplay("8")}>
-            8
-          </button>
-          <button className={style.number} onClick={() => handleDisplay("9")}>
-            9
-          </button>
-          <button className={style.number} onClick={() => handleDisplay("4")}>
-            4
-          </button>
-          <button className={style.number} onClick={() => handleDisplay("5")}>
-            5
-          </button>
-          <button className={style.number} onClick={() => handleDisplay("6")}>
-            6
-          </button>
-          <button className={style.number} onClick={() => handleDisplay("1")}>
-            1
-          </button>
-          <button className={style.number} onClick={() => handleDisplay("2")}>
-            2
-          </button>
-          <button className={style.number} onClick={() => handleDisplay("3")}>
-            3
-          </button>
-          <button className={style.number} onClick={() => handleDisplay(".")}>
-            .
-          </button>
-          <button className={style.number} onClick={() => handleDisplay("0")}>
-            0
-          </button>
-          <button className={style.number} onClick={handleReset}>
-            C
-          </button>
-          <button className={style.equal} onClick={handleCalculate}>
-            =
-          </button>
+          <Button
+            text={"+"}
+            style={style.operator}
+            onClick={() => handleDisplay("+")}
+          />
+          <Button
+            text={"-"}
+            style={style.operator}
+            onClick={() => handleDisplay("-")}
+          />
+          <Button
+            text={"*"}
+            style={style.operator}
+            onClick={() => handleDisplay("*")}
+          />
+          <Button
+            text={"÷"}
+            style={style.operator}
+            onClick={() => handleDisplay("/")}
+          />
+          <Button
+            text={"7"}
+            style={style.number}
+            onClick={() => handleDisplay("7")}
+          />
+          <Button
+            text={"8"}
+            style={style.number}
+            onClick={() => handleDisplay("8")}
+          />
+          <Button
+            text={"9"}
+            style={style.number}
+            onClick={() => handleDisplay("9")}
+          />
+          <Button
+            text={"4"}
+            style={style.number}
+            onClick={() => handleDisplay("4")}
+          />
+          <Button
+            text={"5"}
+            style={style.number}
+            onClick={() => handleDisplay("5")}
+          />
+          <Button
+            text={"6"}
+            style={style.number}
+            onClick={() => handleDisplay("6")}
+          />
+          <Button
+            text={"1"}
+            style={style.number}
+            onClick={() => handleDisplay("1")}
+          />
+          <Button
+            text={"2"}
+            style={style.number}
+            onClick={() => handleDisplay("2")}
+          />
+          <Button
+            text={"3"}
+            style={style.number}
+            onClick={() => handleDisplay("3")}
+          />
+          <Button
+            text={"."}
+            style={style.number}
+            onClick={() => handleDisplay(".")}
+          />
+          <Button
+            text={"0"}
+            style={style.number}
+            onClick={() => handleDisplay("0")}
+          />
+          <Button text={"C"} style={style.number} onClick={handleReset} />
+          <Button text={"="} style={style.equal} onClick={handleCalculate} />
         </div>
       </div>
     </div>
