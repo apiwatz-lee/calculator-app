6751a3a64ec2a13745a055bf95d8e19bf0e15cf4
Apiwat Lee <apiwatlee@Apiwats-MacBook-Air.local>
2023-12-01 13:50:07 +0700

    fix:refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import "./App.css";
 import Button from "./components/Button";
-import useCalculator from "./hook/useCalculator";
+import { useState } from "react";
 
 function App() {
-  const { input, style, handleDisplay, handleReset, handleCalculate } =
-    useCalculator();
+  const [input, setInput] = useState("0");
+
+  const style = {
+    operator:
+      "border border-gray-200 rounded-lg bg-orange-400 text-white text-3xl hover:bg-orange-500 active:-translate-y-5 duration-500",
+    number:
+      "border border-gray-200 rounded-lg bg-gray-200 text-3xl hover:bg-gray-300 active:-translate-y-5 duration-500",
+    equal:
+      "border border-gray-200 rounded-lg col-start-4 row-start-2 row-end-6 bg-orange-400 text-white text-3xl hover:bg-orange-500 active:-translate-y-5 duration-500 ",
+  };
+
+  const handleDisplay = (value) => {
+    if (input.toString().charAt(0) === "0") {
+      setInput(value);
+    } else {
+      setInput((prev) => prev + value);
+    }
+  };
+
+  const handleReset = () => {
+    setInput("0");
+  };
+
+  const handleCalculate = () => {
+    const result = eval(input);
+    setInput(result);
+  };
 
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-slate-200 ">
@@ -88,8 +113,16 @@ function App() {
             style={style.number}
             onClick={() => handleDisplay("0")}
           />
-          <Button text={"C"} style={style.number} onClick={handleReset} />
-          <Button text={"="} style={style.equal} onClick={handleCalculate} />
+          <Button 
+            text={"C"} 
+            style={style.number} 
+            onClick={handleReset} 
+          />
+          <Button 
+            text={"="} 
+            style={style.equal} 
+            onClick={handleCalculate} 
+          />
         </div>
       </div>
     </div>
